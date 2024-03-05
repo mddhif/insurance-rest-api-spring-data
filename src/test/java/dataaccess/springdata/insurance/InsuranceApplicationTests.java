f0d599d456b5e129a5109706a1ebc6910dd8aba3
Mohammed Dhif <4957312+mddhif@users.noreply.github.com>
2024-03-05 11:18:21 +0700

    update...

---
+++ src/test/java/dataaccess/springdata/insurance/InsuranceApplicationTests.java
@@ -0,0 +1,15 @@
+package dataaccess.springdata.insurance;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import static junit.framework.Assert.assertTrue;
+
+//@SpringBootTest
+class InsuranceApplicationTests {
+
+//	@Test
+	public void insuranceTest() {
+//		assertTrue(true);
+	}
+
+}
